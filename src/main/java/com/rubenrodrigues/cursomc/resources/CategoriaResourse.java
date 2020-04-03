56f0d684dfdca35d21ac74abf88dd6fa48a9c220
Ruben C. Rodrigues <rubenrodriguesc@hotmail.com>
2020-04-02 23:48:01 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/rubenrodrigues/cursomc/resources/CategoriaResourse.java
+++ src/main/java/com/rubenrodrigues/cursomc/resources/CategoriaResourse.java
@@ -1,6 +1,8 @@
 package com.rubenrodrigues.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,41 +17,48 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rubenrodrigues.cursomc.domain.Categoria;
+import com.rubenrodrigues.cursomc.domain.dto.CategoriaDTO;
 import com.rubenrodrigues.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResourse {
-	
+
 	@Autowired
 	private CategoriaService service;
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
-		
+
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
 		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Categoria> delete(@PathVariable Integer id) {
-		service.delete(id);	
+		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
 }
