5ad29c7dd952f8e0f7890966702406eb7c04245b
Ruben C. Rodrigues <rubenrodriguesc@hotmail.com>
2020-04-10 12:03:21 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

---
+++ src/main/java/com/rubenrodrigues/cursomc/services/validations/ClienteInsertValidador.java
@@ -0,0 +1,40 @@
+package com.rubenrodrigues.cursomc.services.validations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.rubenrodrigues.cursomc.domain.dto.ClienteNewDTO;
+import com.rubenrodrigues.cursomc.domain.enums.TipoCliente;
+import com.rubenrodrigues.cursomc.resources.exceptions.FieldMessage;
+import com.rubenrodrigues.cursomc.services.validations.utils.BR;
+
+public class ClienteInsertValidador implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert constraintAnnotation) {
+	}
+	
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfCnpj())) {
+			list.add(new FieldMessage("cpfCnpj", "CPF inválido"));
+		}
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfCnpj())) {
+			list.add(new FieldMessage("cpfCnpj", "CNPJ inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+
+		return list.isEmpty();
+	}
+
+}
