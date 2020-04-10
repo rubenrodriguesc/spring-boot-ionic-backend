dd3eae140c8acd55f8eca4ae0f38576585247d0b
Ruben C. Rodrigues <rubenrodriguesc@hotmail.com>
2020-04-10 10:45:40 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/rubenrodrigues/cursomc/services/ClienteService.java
+++ src/main/java/com/rubenrodrigues/cursomc/services/ClienteService.java
@@ -49,7 +49,7 @@ public class ClienteService {
 		try {
 			repository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas.");
+			throw new DataIntegrityException("Não é possível excluir. O Cliente possui pedidos.");
 		}
 	}
 
