7c10eb6547e934f3ec23dd4148482807311c894f
Ruben C. Rodrigues <rubenrodriguesc@hotmail.com>
2020-04-13 22:31:55 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/rubenrodrigues/cursomc/resources/utils/URL.java
@@ -0,0 +1,23 @@
+package com.rubenrodrigues.cursomc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class URL {
+
+	public static List<Integer> decodeIntList(String str) {
+		return Arrays.asList(str.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+	}
+
+	public static String decodeParam(String str) {
+		try {
+			return URLDecoder.decode(str, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+
+}
