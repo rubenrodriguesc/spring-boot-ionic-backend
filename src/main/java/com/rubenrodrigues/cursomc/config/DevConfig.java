a437038fd0f36dba1b2a3f7a0a7a2e7a5cb1a233
Ruben C. Rodrigues <rubenrodriguesc@hotmail.com>
2020-04-19 00:34:15 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/rubenrodrigues/cursomc/config/DevConfig.java
@@ -0,0 +1,30 @@
+package com.rubenrodrigues.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.rubenrodrigues.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instatiateDatabase() {
+		
+		if(!"create".equals(strategy))
+			return false;
+		
+		dbService.instantiateDatabase();
+		return true;
+	}
+}
