ffacebab840b477469f5458117dca4e4767736e9
Ruben C. Rodrigues <rubenrodriguesc@hotmail.com>
2020-04-18 22:02:27 -0300

    Criando profile de teste

---
+++ src/main/java/com/rubenrodrigues/cursomc/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.rubenrodrigues.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.rubenrodrigues.cursomc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Bean
+	public boolean instatiateDatabase() {
+
+		dbService.instantiateDatabase();
+		return true;
+	}
+}
