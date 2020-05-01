51d39b1e75bd7da12af8d1c3245a4b3842b58fca
Ruben C. Rodrigues <rubenrodriguesc@hotmail.com>
2020-05-01 13:27:26 -0300

    Email HTML

--- src/main/java/com/rubenrodrigues/cursomc/services/PedidoService.java
+++ src/main/java/com/rubenrodrigues/cursomc/services/PedidoService.java
@@ -62,7 +62,7 @@ public class PedidoService {
 			itemPedido.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 
